c7ddd51c2b6b315e6baf2bdfeba5336281295b68
Thai Duy <chestraxx@gmail.com>
2023-05-16 18:01:50 +0700

    init menubar

---
+++ src-electron/electron-main.ts
@@ -0,0 +1,95 @@
+import { app, nativeTheme, ipcMain } from 'electron';
+import path from 'path';
+import os from 'os';
+const { menubar } = require('menubar');
+
+// needed in case process is undefined under Linux
+const platform = process.platform || os.platform();
+
+try {
+  if (platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
+    require('fs').unlinkSync(
+      path.join(app.getPath('userData'), 'DevTools Extensions')
+    );
+  }
+} catch (_) {}
+
+const mb = menubar({
+  index: process.env.APP_URL,
+  icon: path.resolve(__dirname, 'icons/MenubarPlusIconTemplate.png'),
+  showDockIcon: false,
+  browserWindow: {
+    width: 270,
+    height: 170,
+    minWidth: 270,
+    minHeight: 170,
+    maxWidth: 270,
+    maxHeight: 170,
+    useContentSize: true,
+    webPreferences: {
+      contextIsolation: true,
+      // More info: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/electron-preload-script
+      preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD),
+    },
+  },
+});
+mb.on('ready', () => {
+  console.log('app is ready');
+});
+
+ipcMain.handle('quit-app', () => {
+  app.quit();
+});
+
+// let mainWindow: BrowserWindow | undefined;
+
+// function createWindow() {
+//   /**
+//    * Initial window options
+//    */
+//   mainWindow = new BrowserWindow({
+//     icon: path.resolve(__dirname, 'icons/icon.png'), // tray icon
+//     width: 270,
+//     height: 170,
+//     minWidth: 270,
+//     minHeight: 170,
+//     maxWidth: 270,
+//     maxHeight: 170,
+//     useContentSize: true,
+//     webPreferences: {
+//       contextIsolation: true,
+//       // More info: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/electron-preload-script
+//       preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD),
+//     },
+//   });
+
+//   mainWindow.loadURL(process.env.APP_URL);
+
+//   if (process.env.DEBUGGING) {
+//     // if on DEV or Production with debug enabled
+//     mainWindow.webContents.openDevTools();
+//   } else {
+//     // we're on production; no access to devtools pls
+//     mainWindow.webContents.on('devtools-opened', () => {
+//       mainWindow?.webContents.closeDevTools();
+//     });
+//   }
+
+//   mainWindow.on('closed', () => {
+//     mainWindow = undefined;
+//   });
+// }
+
+// app.whenReady().then(createWindow);
+
+// app.on('window-all-closed', () => {
+//   if (platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
+
+// app.on('activate', () => {
+//   if (mainWindow === undefined) {
+//     createWindow();
+//   }
+// });
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-btn
+      color="primary"
+      :label="counter"
+      size="xl"
+      class="full-width q-mb-md"
+    />
+
+    <div class="row text-center">
+      <div class="col">
+        <q-btn
+          flat
+          round
+          color="negative"
+          icon="remove"
+          size="lg"
+          @click="decreaseCounter"
+        />
+      </div>
+      <div class="col">
+        <q-btn
+          flat
+          round
+          color=""
+          icon="restart_alt"
+          size="lg"
+          @click="resetCounter"
+        />
+      </div>
+      <div class="col">
+        <q-btn
+          flat
+          round
+          color="positive"
+          icon="add"
+          size="lg"
+          @click="increaseCounter"
+        />
+      </div>
+    </div>
+
+    <q-btn
+      flat
+      round
+      icon="power_settings_new"
+      size="sm"
+      class="absolute-bottom-right"
+      @click="quitApp"
+    />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let counter = ref(0);
+
+const increaseCounter = () => {
+  counter.value++;
+};
+
+const decreaseCounter = () => {
+  counter.value--;
+};
+
+const resetCounter = () => {
+  counter.value = 0;
+};
+
+const quitApp = () => {
+  window.myAPI.quitApp();
+};
+</script>
